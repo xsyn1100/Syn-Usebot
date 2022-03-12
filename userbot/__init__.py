332e6692458cab30fad3d6316774b7e817710ce8
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-03-12 15:19:30 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -81,7 +81,7 @@ PM_LIMIT = int(os.environ.get("PM_LIMIT", 6))
 # Custom Pmpermit pic
 PMPERMIT_PIC = (
     os.environ.get("PMPERMIT_PIC", None)
-    or "https://telegra.ph/file/99a845d405ce19278b179.jpg"
+    or "https://telegra.ph/file/97d841bb7bd5e5969d527.jpg"
 )
 
 # Bleep Blop, this is a bot ;)
@@ -209,11 +209,11 @@ S_PACK_NAME = os.environ.get("S_PACK_NAME", None)
 
 # Default .alive Logo
 ALIVE_LOGO = (os.environ.get("ALIVE_LOGO")
-              or "https://telegra.ph/file/fd08937c4ae6cb1303731.jpg")
+              or "https://telegra.ph/file/97d841bb7bd5e5969d527.jpg")
 
 # Default .helpme Logo
 INLINE_PIC = (os.environ.get("INLINE_PIC")
-              or "https://telegra.ph/file/fd08937c4ae6cb1303731.jpg")
+              or "https://telegra.ph/file/97d841bb7bd5e5969d527.jpg")
 
 # Default emoji help
 EMOJI_HELP = os.environ.get("EMOJI_HELP") or "✗"
